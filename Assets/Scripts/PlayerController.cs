92ba1f7c00fb94bd98c58ff7c7fec3997055535c
Silvio_Unusual <sjimenez@unusualwonder.com>
2016-07-22 15:57:01 +0200

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+
+	public Transform cameraRef;
+	public float MoveSpeed = 5;
+	// Use this for initialization
+	void Start () {
+		charCont = GetComponent<CharacterController>();
+		_initiated = cameraRef != null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_initiated) {
+			Vector3 movement = Vector3.zero;
+#if UNITY_EDITOR
+			if (Input.GetKey(KeyCode.UpArrow))
+			{
+				movement += cameraRef.forward;
+			}
+			else if (Input.GetKey(KeyCode.DownArrow))
+			{
+				movement -= cameraRef.forward;
+			}
+			if (Input.GetKey(KeyCode.RightArrow))
+			{
+				movement += cameraRef.right;
+			}
+			else if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				movement -= cameraRef.right;
+			}
+#else
+			if (Input.touchCount == 1) {
+				movement += cameraRef.forward;
+			}
+#endif
+			movement.y = 0;
+			movement.Normalize();
+			charCont.SimpleMove(movement * MoveSpeed);	//needs speed vector, not displacement vector
+		}
+	}
+
+	private CharacterController charCont;
+	private bool _initiated;
+}
---
+++ Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		System.Random rand = new System.Random();
+		_up = ((float)rand.NextDouble() - 0.5f) * 360;
+		_right = ((float)rand.NextDouble() - 0.5f) * 360 * 2;
+		_forward = ((float)rand.NextDouble() - 0.5f) * 360 * 3;
+		Debug.Log(_up + ", " + _right);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(transform.up, _up * Time.deltaTime);
+		transform.Rotate(transform.right, _right * Time.deltaTime);
+		transform.Rotate(transform.forward, _forward * Time.deltaTime);
+	}
+
+	private float _up, _right, _forward;
+}
